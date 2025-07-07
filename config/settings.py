7a48b2b2d5698abbf5db8c8629f8ca647e48f082
Kristina-Rozhkova <rozkovakristina67@gmail.com>
2025-07-07 15:28:01 +0400

    homework 33 done

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
 
     "drf_yasg",
+    "django_celery_beat",
 
     "users",
     "materials",
@@ -119,3 +120,31 @@ SIMPLE_JWT = {
 }
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
+
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+CELERY_BEAT_SCHEDULE = {
+    "deactivate_inactive_users": {
+        "task": "users.tasks.deactivate_inactive_users",
+        "schedule": timedelta(weeks=4),
+    },
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
+EMAIL_USE_TLS = True if  os.getenv('EMAIL_USE_TLS') == 'True' else False
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = os.getenv('SERVER_EMAIL')
+
+ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
---
+++ users/tasks.py
@@ -0,0 +1,20 @@
+from celery import shared_task
+from .models import User
+from django.utils import timezone
+from datetime import timedelta
+
+@shared_task
+def deactivate_inactive_users():
+    """Блокировка пользователей, которые не заходили более месяца"""
+    month_ago = timezone.now() - timedelta(days=30)
+    count = 0
+
+    filter_login = {"last_login__lte": month_ago, "is_active": True}
+    inactive_users = User.objects.filter(**filter_login)
+
+    if inactive_users.exists():
+        for user in inactive_users:
+            user.update(is_active=False)
+            count += 1
+
+    return f"Заблокировано {count} неактивных пользователей"
